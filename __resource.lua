010443fc64f293bbb26ff85c2dbe4132596a918e
Kevin <34973659+keketiger@users.noreply.github.com>
2019-10-08 18:11:24 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,6 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX FBI Job, https://github.com/ESX-FRANCE/esx_fbi_job'
 version '0.0.3'
-author 'keketiger'
 
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
