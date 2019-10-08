c08cec8fb25ef18ea7b40d4c9193bcab064ab4d5
Kevin <34973659+keketiger@users.noreply.github.com>
2019-10-08 17:58:23 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,7 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX FBI Job, https://github.com/ESX-FRANCE/esx_fbi_job'
-version '0.0.2'
+version '0.0.3'
 author 'keketiger'
 
 server_scripts {
