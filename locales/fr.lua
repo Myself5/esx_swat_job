2f0c204b174013d8d73df06c6878585111ffe477
Kevin <34973659+keketiger@users.noreply.github.com>
2020-02-06 15:39:56 +0100

    Update fr.lua

--- locales/fr.lua
+++ locales/fr.lua
@@ -148,7 +148,6 @@ Locales['fr'] = {
   ['sex'] = 'sexe: %s',
   ['dob'] = 'DOB: %s',
   ['height'] = 'taille: %s',
-  ['id'] = 'ID: %s',
   ['bac'] = 'BAC: %s',
   ['unknown'] = 'inconnu',
   ['male'] = 'homme',
